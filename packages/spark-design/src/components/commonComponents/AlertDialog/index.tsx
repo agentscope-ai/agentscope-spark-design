7cafd336f32559faf54b1733f4d9f9dd4eb1bfa5
wangqifeng <wangqifeng.wqf@alibaba-inc.com>
2025-11-20 13:58:58 +0800

    fix: 新增 AgentScope runtime frontend package

--- packages/spark-design/src/components/commonComponents/AlertDialog/index.tsx
+++ packages/spark-design/src/components/commonComponents/AlertDialog/index.tsx
@@ -1,7 +1,17 @@
 import PCAlertDialog from './pc';
 import MobileAlertDialog from './mobile';
+import type { AlertDialogProps } from './pc';
 
-const AlertDialog = MOBILE ? MobileAlertDialog : PCAlertDialog;
+type AlertDialogStaticProps = Omit<AlertDialogProps, 'type' | 'open'>;
+
+const AlertDialog: {
+  (props: AlertDialogProps): JSX.Element;
+  success: (props?: AlertDialogStaticProps) => void;
+  info: (props?: AlertDialogStaticProps) => void;
+  warning: (props?: AlertDialogStaticProps) => void;
+  error: (props?: AlertDialogStaticProps) => void;
+  confirm: (props?: AlertDialogStaticProps) => void;
+} = MOBILE ? MobileAlertDialog : PCAlertDialog;
 
 export default AlertDialog;
-export type { AlertDialogProps } from './pc';
+export type { AlertDialogProps };
