ddea98caf4190eb9daf8f3a2378fcc00a669b812
JohnKeating1997 <JohnKeatinghhh@gmail.com>
2025-12-08 20:06:07 +0800

    init spark flow

---
+++ packages/spark-flow/src/components/flow-tools/index.tsx
@@ -0,0 +1,34 @@
+import { Divider } from 'antd';
+import React, { memo } from 'react';
+import AddNodeBtn from './add-node-btn';
+import HistoryBtn from './history-btn';
+import './index.less';
+import LayoutBtn from './layout-btn';
+import MiniMapBtn from './mini-map-btn';
+import ScaleBtn from './scale-btn';
+import ShortKeyBtn from './short-key-btn';
+import TouchModeBtn from './touch-mode-btn';
+
+export default memo(function FlowTools() {
+  return (
+    <div
+      id="spark-flow-bottom-tools-container"
+      className="nopan absolute items-center flex left-[16px] bottom-[16px] gap-[12px]"
+    >
+      <div className="spark-flow-tools gap-[8px] items-center flex">
+        <MiniMapBtn />
+        <Divider type="vertical" className="m-0" />
+        <ScaleBtn />
+      </div>
+      <div className="spark-flow-tools gap-[8px] items-center flex">
+        <AddNodeBtn />
+        <LayoutBtn />
+        <Divider type="vertical" className="m-0" />
+        <TouchModeBtn />
+        <Divider type="vertical" className="m-0" />
+        <ShortKeyBtn />
+      </div>
+      <HistoryBtn />
+    </div>
+  );
+});
