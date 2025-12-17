869ef0b8acb9fefd3ba1a21c05ba7df43ee653fa
wangqifeng <wangqifeng.wqf@alibaba-inc.com>
2025-12-17 15:38:03 +0800

    fix: theme prop in CodeBlock functionality updated

--- packages/spark-design/src/components/commonComponents/CodeBlock/demo/demo1.tsx
+++ packages/spark-design/src/components/commonComponents/CodeBlock/demo/demo1.tsx
@@ -39,7 +39,7 @@ const App: React.FC = () => {
           />
         }
       >
-        <CodeBlock language={'javascript'} value={value} theme="dark" />
+        <CodeBlock language={'javascript'} value={value} />
       </CollapsePanel>
     </Flex>
   );
