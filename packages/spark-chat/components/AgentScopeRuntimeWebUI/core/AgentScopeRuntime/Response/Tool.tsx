f4a76980d17f56dcceb9689cf63426a3b0027bfb
lishengzxc <306009337@qq.com>
2026-01-04 20:58:18 +0800

    fix(Tool): refine loading state logic in Tool component
    - Updated the loading condition to ensure it accurately reflects the state of content and data status.
    - Adjusted title rendering to handle undefined values, improving the robustness of the Tool component's display.

--- packages/spark-chat/components/AgentScopeRuntimeWebUI/core/AgentScopeRuntime/Response/Tool.tsx
+++ packages/spark-chat/components/AgentScopeRuntimeWebUI/core/AgentScopeRuntime/Response/Tool.tsx
@@ -13,7 +13,7 @@ const Tool = React.memo(function ({ data }: { data: IAgentScopeRuntimeMessage })
     arguments: Record<string, any>;
     output: Record<string, any>;
   }>[]
-  const loading = !content[1]?.data || data.status === AgentScopeRuntimeRunStatus.InProgress;
+  const loading = !content[1]?.data && data.status === AgentScopeRuntimeRunStatus.InProgress;
   const toolName = content[0].data.name;
   const serverLabel = `${content[0].data.server_label ? content[0].data.server_label + ' / ' : ''}`
   const title = `${serverLabel}${toolName}`
@@ -22,7 +22,7 @@ const Tool = React.memo(function ({ data }: { data: IAgentScopeRuntimeMessage })
     const C = customToolRenderConfig[toolName];
     return <C data={data} />
   } else {
-    return <ToolCall loading={loading} defaultOpen={false} title={title} input={content[0]?.data?.arguments} output={content[1]?.data?.output}></ToolCall>
+    return <ToolCall loading={loading} defaultOpen={false} title={title === 'undefined' ? '' : title} input={content[0]?.data?.arguments} output={content[1]?.data?.output}></ToolCall>
   }
 })
 
