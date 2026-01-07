25effcf580010cabad2aeacdde6a0642ada3b2bc
liuxuely <luoyun.lx0626@gmail.com>
2026-01-07 19:26:08 +0800

    style(Tooltip): update tooltip structure for improved styling
    - Refactored the tooltip styles to include a new class for the tooltip wrapper, enhancing the organization of styles.
    - Maintained existing styles for tooltip content and inner elements to ensure consistent appearance.

--- packages/spark-design/src/components/commonComponents/Tooltip/index.style.ts
+++ packages/spark-design/src/components/commonComponents/Tooltip/index.style.ts
@@ -2,12 +2,14 @@ import createGlobalStyle from '@/libs/createStyle';
 
 export const useStyle = createGlobalStyle`
 /* Default dark mode */
-.${(p) => p.antPrefix}-tooltip-content {
-  .${(p) => p.antPrefix}-tooltip-inner {
-    border: 1px solid transparent;
-    padding: 6px 12px;
-    background: var(--${(p) => p.antPrefix}-color-slate);
-    color: var(--${(p) => p.antPrefix}-color-text-white);
+.${(p) => p.antPrefix}-tooltip {
+  .${(p) => p.antPrefix}-tooltip-content {
+    .${(p) => p.antPrefix}-tooltip-inner {
+      border: 1px solid transparent;
+      padding: 6px 12px;
+      background: var(--${(p) => p.antPrefix}-color-slate);
+      color: var(--${(p) => p.antPrefix}-color-text-white);
+    }
   }
 }
 
