e4cf6d8a549f0cb3cd0867a65ca2ff986e1cb05e
lishengzxc <306009337@qq.com>
2026-01-11 14:52:54 +0800

    feat(AssetsPreview): enhance video component with playback state and styling improvements
    - Added support for displaying current playback time and updated play/pause button with appropriate icons.
    - Improved overlay styles for video playback states (playing/paused) to enhance user interaction.
    - Updated demo to showcase new image previews alongside video previews for better visual representation.

--- packages/spark-chat/components/AssetsPreview/demo/index.tsx
+++ packages/spark-chat/components/AssetsPreview/demo/index.tsx
@@ -28,6 +28,19 @@ export default function () {
       { src: 'https://img.alicdn.com/imgextra/i3/O1CN01dKprif1ar0awCMDwK_!!6000000003382-0-tps-2640-1100.jpg', width: 16, height: 9 },
     ]} />
 
+    <AssetsPreview type="image" data={[
+      { src: 'https://img.alicdn.com/imgextra/i3/O1CN01dKprif1ar0awCMDwK_!!6000000003382-0-tps-2640-1100.jpg', width: 1, height: 1 },
+      { src: 'https://img.alicdn.com/imgextra/i3/O1CN01dKprif1ar0awCMDwK_!!6000000003382-0-tps-2640-1100.jpg', width: 1, height: 1 },
+      { src: 'https://img.alicdn.com/imgextra/i3/O1CN01dKprif1ar0awCMDwK_!!6000000003382-0-tps-2640-1100.jpg', width: 1, height: 1 },
+      { src: 'https://img.alicdn.com/imgextra/i3/O1CN01dKprif1ar0awCMDwK_!!6000000003382-0-tps-2640-1100.jpg', width: 1, height: 1 },
+    ]} />
+
+<AssetsPreview type="image" data={[
+      { src: 'https://img.alicdn.com/imgextra/i3/O1CN01dKprif1ar0awCMDwK_!!6000000003382-0-tps-2640-1100.jpg', width: 9, height: 16 },
+      { src: 'https://img.alicdn.com/imgextra/i3/O1CN01dKprif1ar0awCMDwK_!!6000000003382-0-tps-2640-1100.jpg', width: 9, height: 16 },
+      { src: 'https://img.alicdn.com/imgextra/i3/O1CN01dKprif1ar0awCMDwK_!!6000000003382-0-tps-2640-1100.jpg', width: 9, height: 16 },
+    ]} />
+
     <AssetsPreview type="video" data={[
       { src: 'https://cloud.video.taobao.com/vod/m9amjbqLTGUvaGo3o61u-Ch2hycUCa3RA3pAw1-Zv_0.mp4', width: 16, height: 9 },
     ]} />
