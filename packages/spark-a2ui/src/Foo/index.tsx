12fda1476a648f93b525d8fd4c6a0b2ab5eb864b
lishengzxc <306009337@qq.com>
2026-01-13 10:30:31 +0800

    feat(Foo): enhance App component with state management and example selection
    - Added state management to the App component using useState to allow dynamic selection of example messages.
    - Integrated Ant Design's Select component for user-friendly example selection, improving the demonstration of A2UI functionality.
    - Updated the rendering logic to reflect the selected example, enhancing the overall user experience.

--- packages/spark-a2ui/src/Foo/index.tsx
+++ packages/spark-a2ui/src/Foo/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   A2UIProvider,
@@ -8,19 +8,25 @@ import {
 } from '@agentscope-ai/spark-a2ui'
 
 import examples from './mock'
+import { Select } from 'antd';
 
 
 function App() {
-  const messages: A2UIMessage[] = examples[0].messages;
+
+  const [index, setIndex] = useState(0);
+  const messages: A2UIMessage[] = examples[index].messages;
 
   const handleAction = (action: A2UIAction) => {
     console.log('Action received:', action)
   }
 
   return (
-    <A2UIProvider messages={messages}>
-      <A2UIRenderer onAction={handleAction} />
-    </A2UIProvider>
+    <>
+      <Select options={examples.map((example, index) => ({ label: example.title, value: index }))} value={index} onChange={setIndex} />
+      <A2UIProvider messages={messages}>
+        <A2UIRenderer onAction={handleAction} />
+      </A2UIProvider>
+    </>
   )
 }
 
