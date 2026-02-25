85811415cf52ceabb43a18ebdee2f1388ee51c9e
wangqifeng <wangqifeng.wqf@alibaba-inc.com>
2026-02-25 17:00:43 +0800

    fix: aone-pages build dir fix

--- packages/spark-design/src/components/commonComponents/PromptsEditor/demo/basic.tsx
+++ packages/spark-design/src/components/commonComponents/PromptsEditor/demo/basic.tsx
@@ -2,6 +2,7 @@ import { PromptsEditor } from '@agentscope-ai/design';
 import { useState } from 'react';
 
 export default function Basic() {
+  
   const [value, setValue] = useState(`# 角色
 你是一个专业的Prompt编写工程师，擅长编写各种类型的Prompt。
 
@@ -28,6 +29,7 @@ export default function Basic() {
       value={value}
       onChange={setValue}
       maxLength={5000}
+      className="promptsEditor-demo"
       variables={[
         { code: 'city' },
         { code: 'foo' },
