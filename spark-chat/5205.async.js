1ddb0cc52ce747e1393a02a7ef7e5bab91a1c816
lishengzxc <lishengzxc@users.noreply.github.com>
2026-04-01 07:21:44 +0000

    deploy: 6cc6826a7dfef3f0f91e081577b6a7d0f7cf93c2

--- spark-chat/5205.async.js
+++ spark-chat/5205.async.js
@@ -2021,7 +2021,7 @@ import { ChatInput } from '@agentscope-ai/chat';
 
 export default function () {
   const [value, setValue] = useState('Hello, Alibaba Cloud Spark Chat!');
-  return <ChatInput placeholder='Please type here...' value={value} onChange={setValue}></ChatInput>
+  return <ChatInput placeholder='Please type here...' value={value} onChange={setValue} maxLength={100}></ChatInput>
 }`},16864:function(e,n){n.Z=`import { ChatInput } from '@agentscope-ai/chat';
 
 export default function () {
