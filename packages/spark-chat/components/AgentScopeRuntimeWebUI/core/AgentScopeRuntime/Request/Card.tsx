98f0eeef84a3084f687063c2e424de9795941bf0
wangqifeng <wangqifeng.wqf@alibaba-inc.com>
2026-05-19 17:50:52 +0800

    fix: update

--- packages/spark-chat/components/AgentScopeRuntimeWebUI/core/AgentScopeRuntime/Request/Card.tsx
+++ packages/spark-chat/components/AgentScopeRuntimeWebUI/core/AgentScopeRuntime/Request/Card.tsx
@@ -74,7 +74,7 @@ export default function AgentScopeRuntimeRequestCard(props: {
       }
       return p;
     }, []);
-  }, [props.data.input, onFileClick]);
+  }, [props.data.input, onFileCardClick]);
 
   if (!cards?.length) return null;
 
